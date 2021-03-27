7d5e813086b49b4eaf3eefeabfbebf1a4a1d7d44
Thomas L. Andersen <thomas-leschley@hotmail.com>
2021-03-27 17:58:41 +0100

    update readme

--- src/LHSBrackets.ModelBinder.Example/Controllers/BooksFilterRequest.cs
+++ src/LHSBrackets.ModelBinder.Example/Controllers/BooksFilterRequest.cs
@@ -4,28 +4,29 @@ using LHSBrackets.ModelBinder.Example.Database;
 
 namespace LHSBrackets.ModelBinder.Example.Controllers.Rentals
 {
+    // use nullable types
     public class BooksFilterRequest : FilterRequest
     {
-        public FilterOperations<Guid?> AuthorId { get; set; } = new FilterOperations<Guid?>();
-        public FilterOperations<DateTime?> ReleaseDate { get; set; } = new FilterOperations<DateTime?>();
-        public FilterOperations<int?> CategoryId { get; set; } = new FilterOperations<int?>();
-        public FilterOperations<decimal?> Price { get; set; } = new FilterOperations<decimal?>();
-        public FilterOperations<string> Name { get; set; } = new FilterOperations<string>();
-        public FilterOperations<string> CategoryName { get; set; } = new FilterOperations<string>();
-        public FilterOperations<DifficultyEnum?> Difficulty { get; set; } = new FilterOperations<DifficultyEnum?>();
+        public FilterOperations<Guid?> AuthorId { get; set; }
+        public FilterOperations<DateTime?> ReleaseDate { get; set; }
+        public FilterOperations<int?> CategoryId { get; set; }
+        public FilterOperations<decimal?> Price { get; set; }
+        public FilterOperations<string> Name { get; set; }
+        public FilterOperations<string> CategoryName { get; set; }
+        public FilterOperations<DifficultyEnum?> Difficulty { get; set; }
 
 
-        public override IEnumerable<(string PropertyName, Action<string> BindValue)> GetBinders()
+        public override IEnumerable<(string PropertyName, Action<string> BindValue)> GetPropertyBinders()
         {
             var binders = new List<(string PropertyName, Action<string> BindValue)>();
 
-            binders.AddRange(BuildFilterOperationBinders(AuthorId, nameof(AuthorId)));
-            binders.AddRange(BuildFilterOperationBinders(ReleaseDate, nameof(ReleaseDate)));
-            binders.AddRange(BuildFilterOperationBinders(CategoryName, nameof(CategoryName)));
-            binders.AddRange(BuildFilterOperationBinders(Price, nameof(Price)));
-            binders.AddRange(BuildFilterOperationBinders(Name, nameof(Name)));
-            binders.AddRange(BuildFilterOperationBinders(CategoryId, nameof(CategoryId)));
-            binders.AddRange(BuildFilterOperationBinders(Difficulty, nameof(Difficulty)));
+            binders.AddRange(base.GetPropertyBinder(AuthorId, nameof(AuthorId)));
+            binders.AddRange(base.GetPropertyBinder(ReleaseDate, nameof(ReleaseDate)));
+            binders.AddRange(base.GetPropertyBinder(CategoryName, nameof(CategoryName)));
+            binders.AddRange(base.GetPropertyBinder(Price, nameof(Price)));
+            binders.AddRange(base.GetPropertyBinder(Name, nameof(Name)));
+            binders.AddRange(base.GetPropertyBinder(CategoryId, nameof(CategoryId)));
+            binders.AddRange(base.GetPropertyBinder(Difficulty, nameof(Difficulty)));
 
             return binders;
         }
